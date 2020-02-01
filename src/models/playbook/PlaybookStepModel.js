dd14db9eb84664d2e9e19eb30e0b11e7e1ddc1f0
Dominic Trang <dominic_t@hotmail.co.nz>
2020-02-01 21:49:22 +1300

    feat(PlaybookModels): Add a bunch of models that will help construct the playbook.js file

---
+++ src/models/playbook/PlaybookStepModel.js
@@ -0,0 +1,89 @@
+/**
+ * @requires Services
+ */
+import * as TextIndentService from '../../services/utils/TextIndentService';
+
+/**
+ * @requires Models
+ */
+import {
+    PlaybookTimelineDescriptionModel, 
+    PlaybookTimelineCodeModel
+} from './PlaybookTimelineModel';
+
+/**
+ * The Step model added to a playbook scene model. This will contain a timeline that 
+ * orders how we play back the playbook
+ *
+ * @class PlaybookStepModel
+ */
+export class PlaybookStepModel
+{
+    name;
+    commitId;
+
+    timelineDescriptionModels = [];
+    timelineCodeModels = [];
+    
+
+    constructor(name, commitId)
+    {
+        this.name = name;
+        this.commitId = commitId;
+    }
+
+    /**
+     * Creates a PlaybookTimelineDescriptionModel that represents ".addDescriptionFromMdFile()" in the playbook.js file
+     *
+     * @param {*} name
+     * @returns {PlaybookTimelineDescriptionModel}
+     * @memberof PlaybookStepModel
+     */
+    addDescriptionFromMdFile(start, duration, filePath)
+    {
+        const timelineDescriptionModel = new PlaybookTimelineDescriptionModel(start, duration, filePath);
+        this.timelineDescriptionModels.push(timelineDescriptionModel);
+
+        return timelineDescriptionModel;
+    }
+
+    /**
+     * Creates a PlaybookTimelineCodeModel that represents ".addCode()" in the playbook.js file
+     *
+     * @param {*} name
+     * @returns {PlaybookTimelineCodeModel}
+     * @memberof PlaybookStepModel
+     */
+    addCode(start, duration, filePath, compileData)
+    {
+        const timelineCodeModel = new PlaybookTimelineCodeModel(start, duration, filePath, compileData);
+        this.timelineCodeModels.push(timelineCodeModel);
+
+        return timelineCodeModel;
+    }
+    
+    /**
+     * Prints the step entry in playbook.js format and any timeline data that is a part of this step
+     *
+     * @param {number} [indentSize=1]
+     * @returns {string}
+     * @memberof PlaybookStepModel
+     */
+    printJsContent(indentSize = 3)
+    {
+        const indent = TextIndentService.indent(indentSize);
+
+        let content = indent + '.addStep("' + this.name + '")\n';
+
+        this.timelineDescriptionModels.forEach((timelineDescriptionModel) => {
+            content += timelineDescriptionModel.printJsContent(indentSize + 1);
+        })
+
+        this.timelineCodeModels.forEach((timelineCodeModel) => {
+            content += timelineCodeModel.printJsContent(indentSize + 1);
+        })
+
+        return content;
+    }
+
+}
